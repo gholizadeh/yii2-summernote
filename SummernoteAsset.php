b32554585164944cc69fa1cd5c97ea884f9682ba
Saeed <s_gholizadeh@aol.com>
2019-01-14 07:10:14 +0330

    Update SummernoteAsset.php

--- SummernoteAsset.php
+++ SummernoteAsset.php
@@ -19,10 +19,8 @@ class SummernoteAsset extends AssetBundle
      */
     public function init()
     {
-        $postfix = YII_DEBUG ? '' : '.min';
-
-        $this->css[] = 'summernote.css';
-        $this->js[] = 'summernote' . $postfix . '.js';
+        $this->css[] = 'summernote-bs4.css';
+        $this->js[] = 'summernote-bs4.js';
 
         parent::init();
     }
